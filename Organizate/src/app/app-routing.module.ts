ef6769c832c111b9bc92ddc9c6368fb763d84d11
javiierssj <javierigp0@gmail.com>
2024-01-06 02:44:18 -0300

    se modifican colores para dejar parecido a mockup

--- Organizate/src/app/app-routing.module.ts
+++ Organizate/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'main',
     loadChildren: () => import('./pages/main/main.module').then( m => m.MainPageModule)
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
 ];
 
 @NgModule({
