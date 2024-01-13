55baf1fb2a4318f0def5ff6e697aaaff6201ed39
javiierssj <jav.guzmanp@ducocuc.cl>
2024-01-12 21:35:12 -0300

    se crea main, se deja funcional login y register

--- Organizate/src/app/app.module.ts
+++ Organizate/src/app/app.module.ts
@@ -10,7 +10,11 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot({mode: 'md'}), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule,  
+    IonicModule.forRoot({mode: 'md'}), 
+    AppRoutingModule, 
+    HttpClientModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
